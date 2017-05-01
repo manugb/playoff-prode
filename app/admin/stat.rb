c5315da5247ada065810deb53e9ff4dd9a8402fc
Manuel Gambino <manuelgambinob@gmail.com>
2017-04-30 23:37:35 -0300

    Empty value for dropdown

--- app/admin/stat.rb
+++ app/admin/stat.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Match do
+ActiveAdmin.register Stat do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -12,13 +12,13 @@ ActiveAdmin.register Match do
 #   permitted
 # end
 
-  permit_params :active, :winner, :most_points, :most_assists, :most_rebounds
-
   index do
     id_column
-    column :serie
-    column :game
-    column :active
+    column :user
+    column :winner
+    column :most_points
+    column :most_assists
+    column :most_rebounds
     actions
   end
 

46b0d3ccef15d822401c6b2cd1e0f7c4fd6bfa07
Manuel Gambino <manuelgambinob@gmail.com>
2017-05-03 01:08:20 -0300

    fix AA match update

--- app/admin/match.rb
+++ app/admin/match.rb
@@ -12,14 +12,20 @@ ActiveAdmin.register Match do
 #   permitted
 # end
 
-  permit_params :active, :winner, :most_points, :most_assists, :most_rebounds
+  permit_params :active, :winner_id, :most_points_id, :most_assists_id, :most_rebounds_id
 
   index do
     id_column
     column :serie
     column :game
     column :active
+    column :winner
+    column :most_points
+    column :most_assists
+    column :most_rebounds
     actions
   end
+
+
 
 end

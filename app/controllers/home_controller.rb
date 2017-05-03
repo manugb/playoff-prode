81ae3d81d0b39a8fda93045cb0c71aaf9d6d40f9
Manuel Gambino <manuelgambinob@gmail.com>
2017-05-03 01:20:23 -0300

    Sort points

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,6 +16,6 @@ class HomeController < ApplicationController
   end
 
   def points
-    @user_points = User.all.map {|u| {name: u.name, points: u.points}}
+    @user_points = User.all.map {|u| {name: u.name, points: u.points}}.sort {|x,y| y[:points] <=> x[:points] }
   end
 end

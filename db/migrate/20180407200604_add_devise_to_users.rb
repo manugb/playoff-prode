81dca08648b585644cacab999c00103538cce78d
Manuel Gambino <manuelgambinob@gmail.com>
2018-04-09 20:30:30 -0300

    Remove null and default constraint to email

--- db/migrate/20180407200604_add_devise_to_users.rb
+++ db/migrate/20180407200604_add_devise_to_users.rb
@@ -4,8 +4,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false
-      t.string :encrypted_password, null: false
+      t.string :email,
+      t.string :encrypted_password,
 
       ## Recoverable
       t.string   :reset_password_token

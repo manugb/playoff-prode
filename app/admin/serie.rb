18c6e248ff73b3add1e5692e73fa84b6182a2404
Manuel Gambino <manuelgambinob@gmail.com>
2018-04-28 11:45:19 -0300

    Add Series in AA

---
+++ app/admin/serie.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Serie do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  permit_params :active, :winner_team_id, :loser_win_games
+
+  index do
+    id_column
+    column :name
+    column :active
+    column :winner_team
+    column :loser_win_games
+    actions
+  end
+
+
+
+end
